973185c2d92525b6350be16e47f52240c8f2621f
fy249085 <fy249085@gongdao.com>
2021-06-02 01:17:20 +0800

    Singleton

--- src/main/java/com/ljy/java/threadlocal/test.java
+++ src/main/java/com/ljy/java/threadlocal/test.java
@@ -8,21 +8,42 @@ public class test {
 
     public static final Integer SIZE  = 20;
 
-    static ThreadLocal<User>    local = new ThreadLocal<>();
+    static ThreadLocal<String>    local = new ThreadLocal();
 
     public static void main(String[] args) {
 
-        try {
-            for (int i = 0; i < SIZE; i++) {
-                local.set(new User("zhangsan" + i, "address" + i));
-            }
-            System.gc();
-            User user = local.get();
-            System.out.println(user);
+        //try {
+        //    for (int i = 0; i < SIZE; i++) {
+        //        local.set(new User("zhangsan" + i, "address" + i));
+        //    }
+        //    System.gc();
+        //    User user = local.get();
+        //    System.out.println(user);
+        //
+        //} catch (Exception e) {
+        //    e.printStackTrace();
+        //}
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        //new Thread(new Runnable() {
+        //    @Override public void run() {
+        //        local.set("zhan高三");
+        //    }
+        //}).start();
+        //
+        //new Thread(new Runnable() {
+        //    @Override public void run() {
+        //        local.set("zhan高二");
+        //    }
+        //}).start();
+        for(int i=0;i<10000;i++){
+            local.set(Math.random()+"ha");
         }
+
+
+
+
+        String s = local.get();
+        System.out.println(s);
 
     }
 
