a2ece46a9d744f5a617241ad30922b26ed3ee39c
fy249085 <fy249085@gongdao.com>
2021-06-26 00:15:16 +0800

    Leetcode70

---
+++ src/main/java/com/ljy/leetcode/num51to100/Leetcode70.java
@@ -0,0 +1,64 @@
+package com.ljy.leetcode.num51to100;
+
+/**
+ * @author fengyue
+ * @date 2021/6/24
+ *
+ * 70. 爬楼梯
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leetcode70 {
+
+    /**
+     * 第n个台阶只能从第n-1或者n-2个上来。到第n-1个台阶的走法 + 第n-2个台阶的走法 = 到第n个台阶的走法，已经知道了第1个和第2个台阶的走法，一路加上去。
+     * 
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (1 == n) {
+            return 1;
+        }
+        if (2 == n) {
+            return 2;
+        }
+
+        int step1 = 1;
+        int step2 = 2;
+        int temp;
+
+        for (int i = 3; i <= n; i++) {
+            temp = step1;
+            step1 = step2;
+            step2 = temp + step2;
+
+        }
+        return step2;
+    }
+
+}
